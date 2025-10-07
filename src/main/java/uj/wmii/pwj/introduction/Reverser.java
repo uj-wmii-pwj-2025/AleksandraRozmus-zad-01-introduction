a941af4d8668280300863721edce15f1e027e27c
Arozm10 <147539234+Arozm10@users.noreply.github.com>
2025-10-07 17:55:39 +0200

    Solution added

--- src/main/java/uj/wmii/pwj/introduction/Reverser.java
+++ src/main/java/uj/wmii/pwj/introduction/Reverser.java
@@ -3,11 +3,31 @@ package uj.wmii.pwj.introduction;
 public class Reverser {
 
     public String reverse(String input) {
-        return null;
+        if (input == null)
+            return null;
+
+        input = input.strip();
+        String reverse = "";
+
+        for (int i = input.length() - 1; i >= 0; i--)
+            reverse += input.charAt(i);
+
+        return reverse;
     }
 
     public String reverseWords(String input) {
-        return null;
-    }
+        if (input == null)
+            return null;
+            
+        input = input.strip();
+        String reverse = "";
+        String[] words = input.split(" ");
 
+        for (int i = 0; i < words.length; i++)
+            reverse = words[i] + " " + reverse;
+
+        reverse = reverse.strip();
+
+        return reverse;
+    }
 }
