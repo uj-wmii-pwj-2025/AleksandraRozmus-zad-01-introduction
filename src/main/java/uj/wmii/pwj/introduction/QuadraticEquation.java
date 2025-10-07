62a18182a0a09e83007de21249ef52661f80d9be
Arozm10 <147539234+Arozm10@users.noreply.github.com>
2025-10-07 17:53:59 +0200

    Solution added

--- src/main/java/uj/wmii/pwj/introduction/QuadraticEquation.java
+++ src/main/java/uj/wmii/pwj/introduction/QuadraticEquation.java
@@ -3,8 +3,16 @@ package uj.wmii.pwj.introduction;
 public class QuadraticEquation {
 
     public double[] findRoots(double a, double b, double c) {
-        return null;
+        double delta = b*b - 4*a*c;
+
+        if (delta < 0)
+            return new double[0];
+        else if (delta == 0)
+            return new double[] {-b/(2*a)};
+        else { 
+            double root1 = (-b - Math.sqrt(delta))/(2*a);
+            double root2 = (-b + Math.sqrt(delta))/(2*a);
+            return new double[] {root1, root2};
+        }
     }
-
 }
-
